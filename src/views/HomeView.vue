0fe68ea0b0bee25302b3e446e6455792c1379c4d
Franciele Lemos Martins <61590835+FrancieleMartins2020@users.noreply.github.com>
2025-06-24 20:32:01 -0300

    feat(HomeView):
     Retirando o carrossel de todas as abas do site, implementar o carrossel apenas no HOME menu principal.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,14 +6,11 @@
       <h1>{{ t('homePage.title') }}</h1>
       <p>{{ t('homePage.welcomeMessage') }}</p>
     </div>
-    <!-- Agora usando o novo rodapé -->
-    <Footer />
   </main>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
-import Footer from "@/components/Footer.vue"; 
 import AppCarousel from "@/components/AppCarousel.vue";
 
 const { t } = useI18n();
