3de7864f64bf0b89d8667bfb6eac717d95839817
Franciele Lemos Martins <61590835+FrancieleMartins2020@users.noreply.github.com>
2025-06-24 21:17:13 -0300

    feat(HomeView):
    feat(HomeView): Recriando o rodapé, para retirar o css e criar novo modelo usandi container e vue.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,12 @@
     <div class="container py-5">
       <h1>{{ t('homePage.title') }}</h1>
       <p>{{ t('homePage.welcomeMessage') }}</p>
-    </div>
-    <FooterBar />
+    </div>    
   </main>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
-import FooterBar from "@/components/FooterBar.vue"; 
 import AppCarousel from "@/components/AppCarousel.vue";
 
 const { t } = useI18n();
